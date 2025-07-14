2dfbb71f4d62793f59a45fca8efaf7109e695ab3
kytran-saritasa <ky.tran@saritasa.com>
2025-07-15 04:51:27 +0700

    Add spiegel fonts

--- src/shared/config/fonts.ts
+++ src/shared/config/fonts.ts
@@ -1,34 +1,85 @@
 import localFont from 'next/font/local';
 
-const riotFontRegular = localFont({
+const riotRegular = localFont({
   src: '../../../public/fonts/Riot/Riot-Regular.woff2',
   variable: '--font-riot-regular',
   display: 'swap',
   fallback: ['Arial', 'sans-serif'],
 });
 
-const riotFontItalic = localFont({
+const riotItalic = localFont({
   src: '../../../public/fonts/Riot/Riot-Regular-Italic.woff2',
   variable: '--font-riot-italic',
   display: 'swap',
   fallback: ['Arial', 'sans-serif'],
 });
 
-const riotFontBold = localFont({
+const riotBold = localFont({
   src: '../../../public/fonts/Riot/Riot-Bold.woff2',
   variable: '--font-riot-bold',
   display: 'swap',
   fallback: ['Arial', 'sans-serif'],
 });
 
-const riotFontBoldItalic = localFont({
+const riotBoldItalic = localFont({
   src: '../../../public/fonts/Riot/Riot-Bold-Italic.woff2',
   variable: '--font-riot-bold-italic',
   display: 'swap',
   fallback: ['Arial', 'sans-serif'],
 });
 
-const riotFonts = [riotFontRegular, riotFontItalic, riotFontBold, riotFontBoldItalic];
+const riotFonts = [riotRegular, riotItalic, riotBold, riotBoldItalic];
+
+const spiegelRegular = localFont({
+  src: '../../../public/fonts/Spiegel/Spiegel-Regular.ttf',
+  variable: '--font-spiegel-regular',
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
+});
+
+const spiegelItalic = localFont({
+  src: '../../../public/fonts/Spiegel/Spiegel-Regular-Italic.ttf',
+  variable: '--font-spiegel-regular-italic',
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
+});
+
+const spiegelSemiBold = localFont({
+  src: '../../../public/fonts/Spiegel/Spiegel-Semi-Bold.ttf',
+  variable: '--font-spiegel-semibold',
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
+});
+
+const spiegelSemiBoldItalic = localFont({
+  src: '../../../public/fonts/Spiegel/Spiegel-Semi-Bold-Italic.ttf',
+  variable: '--font-spiegel-semibold-italic',
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
+});
+
+const spiegelBold = localFont({
+  src: '../../../public/fonts/Spiegel/Spiegel-Bold.ttf',
+  variable: '--font-spiegel-bold',
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
+});
+
+const spiegelBoldItalic = localFont({
+  src: '../../../public/fonts/Spiegel/Spiegel-Bold-Italic.ttf',
+  variable: '--font-spiegel-bold-italic',
+  display: 'swap',
+  fallback: ['Arial', 'sans-serif'],
+});
+
+const spiegelFonts = [
+  spiegelRegular,
+  spiegelItalic,
+  spiegelSemiBold,
+  spiegelSemiBoldItalic,
+  spiegelBold,
+  spiegelBoldItalic,
+];
 
 /** List of fonts. */
-export const fonts = [...riotFonts];
+export const fonts = [...riotFonts, ...spiegelFonts];
