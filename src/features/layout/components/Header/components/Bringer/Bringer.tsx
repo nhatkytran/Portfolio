aa6d0b84064c19b52c3681072111b956d81e6e37
kytran-saritasa <ky.tran@saritasa.com>
2025-07-20 15:22:27 +0700

    Implement more stylish bringer ui

--- src/features/layout/components/Header/components/Bringer/Bringer.tsx
+++ src/features/layout/components/Header/components/Bringer/Bringer.tsx
@@ -1,11 +1,17 @@
 import { BringerIcon } from '@/shared/icons';
+import { cn } from '@/shared/utils/helpers';
 
 /** Bringer - Night / Dawn. */
 export default function Bringer() {
   return (
-    <div className="group flex cursor-pointer items-center gap-1 px-4 py-1.5">
-      <span className="font-riot-bold text-sm tracking-wider uppercase group-hover:text-sky-400">To Dawn</span>
-      <BringerIcon className="size-3.5 group-hover:fill-sky-400" />
+    <div className="group relative flex cursor-pointer items-center gap-1 px-4 py-1.5">
+      <span className="font-riot-bold text-sm tracking-[0.2em] uppercase group-hover:text-sky-400">To.Dawn</span>
+      <BringerIcon
+        className={cn(
+          'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2',
+          'size-12 opacity-20 group-hover:fill-sky-400',
+        )}
+      />
     </div>
   );
 }
