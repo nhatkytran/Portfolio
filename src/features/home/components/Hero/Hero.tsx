a216d795c7a0b345e4c5b917aa7578522e4e126c
kytran-saritasa <ky.tran@saritasa.com>
2025-08-29 11:08:07 +0700

    Prepare flag images

--- src/features/home/components/Hero/Hero.tsx
+++ src/features/home/components/Hero/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
   return (
     <section className="relative bg-cover bg-center bg-no-repeat">
       <div
-        style={{ backgroundImage: "url('/images/hero-bg.png')" }}
+        style={{ backgroundImage: "url('/images/hero/hero-bg.png')" }}
         className={cn('flex items-center justify-center', 'bg-black py-10', 'sm:py-14', 'lg:py-20', 'xl:pb-24')}
       >
         <HeroContent />
