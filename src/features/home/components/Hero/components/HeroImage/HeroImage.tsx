465ad1fadd8ef99778b687fc5aea446484e37aa6
kytran-saritasa <ky.tran@saritasa.com>
2025-11-30 21:06:53 +0700

    Add hero Chinese poem

--- src/features/home/components/Hero/components/HeroImage/HeroImage.tsx
+++ src/features/home/components/Hero/components/HeroImage/HeroImage.tsx
@@ -16,6 +16,22 @@ export default function HomePage() {
       <AnimationImage src="/images/hero/ky-tran.jpg" shouldBeNightbringer />
       <AnimationImage src="/images/hero/ky-tran-light.jpg" shouldBeNightbringer={false} />
       <div className={cn('hero-image-overlay-bg size-[101%]', 'absolute top-0 left-0')} />
+      <div
+        className={cn(
+          'font-beaufort-bold leading-none [text-orientation:upright] [writing-mode:vertical-rl]',
+          'absolute top-[72px] right-[36px] min-h-full text-[20px] text-white/60',
+        )}
+      >
+        少 年 壯 志 懷 天 夢
+      </div>
+      <div
+        className={cn(
+          'font-beaufort-bold leading-none [text-orientation:upright] [writing-mode:vertical-rl]',
+          'absolute top-[138px] right-[82px] min-h-full text-[20px] text-white/60',
+        )}
+      >
+        自 許 人 間 第 一 流
+      </div>
       <BackgroundImage />
     </div>
   );
